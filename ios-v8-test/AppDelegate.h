8fb012de087ca32f48cc2a511644aa50a79ee726
minggo <nudtzxm@gmail.com>
2019-06-06 16:08:26 +0800

    Initial Commit

---
+++ ios-v8-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-v8-test
+//
+//  Created by minggo on 2019/6/6.
+//  Copyright © 2019 minggo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-v8-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-v8-test
+//
+//  Created by minggo on 2019/6/6.
+//  Copyright © 2019 minggo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
